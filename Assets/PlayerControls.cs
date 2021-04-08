544c61d9860c4197fdd04e17e417a116c993adf9
Arhan Busam <arhan.busam@gmail.com>
2021-04-08 10:27:37 +1000

    Updated timeline

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -11,6 +11,16 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float zRange = 5f;
     [SerializeField] float yRange = 5f;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+
+    [SerializeField] float positionYawFactor = 2f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float zThrow;
+    float yThrow;
+    
     void OnEnable()
     {
         movement.Enable();
@@ -30,13 +40,21 @@ public class PlayerControls : MonoBehaviour
 
     private void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(-30f, -90, 0f);
+
+        float positionPitch = transform.localPosition.y * positionPitchFactor;
+        float controlPitch = yThrow * controlPitchFactor;
+
+        float pitch = positionPitch + controlPitch;
+        float yaw = transform.localPosition.z * positionYawFactor;
+        float roll = zThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw + -101.995f, roll);
     }
 
     private void ProcessTranslation()
     {
-        float zThrow = movement.ReadValue<Vector2>().x;
-        float yThrow = movement.ReadValue<Vector2>().y;
+        zThrow = movement.ReadValue<Vector2>().x;
+        yThrow = movement.ReadValue<Vector2>().y;
 
         float zOffset = zThrow * Time.deltaTime * movementSpeed;
         float rawZPos = transform.localPosition.z + zOffset;
