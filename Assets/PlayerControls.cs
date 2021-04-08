3a3607b8303df5e6c606b59837430e79334bb42f
Arhan Busam <arhan.busam@gmail.com>
2021-04-08 15:29:55 +1000

    Particles and Colliders

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,25 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
 
-    [SerializeField] InputAction movement;
-    [SerializeField] float movementSpeed = 1f;
-    [SerializeField] float zRange = 5f;
-    [SerializeField] float yRange = 5f;
+    [Header("Controls")]
+    [Tooltip("The controls for moving the ship")] [SerializeField] InputAction movement;
+    [Tooltip("The button to fire lasers")] [SerializeField] InputAction fire;
 
-    [SerializeField] float positionPitchFactor = -2f;
-    [SerializeField] float controlPitchFactor = -10f;
+    [Header("Movement")]
+    [Tooltip("The speed the rocket moves at")] [SerializeField] float movementSpeed = 1f;
+    [Tooltip("The movement range of the rocket when moving on a horizontal axis")] [SerializeField] float zRange = 5f;
+    [Tooltip("The movement range of the rocket when moving on a vertical axis")] [SerializeField] float yRange = 5f;
 
-    [SerializeField] float positionYawFactor = 2f;
+    [Header("Rotation")]
+    [Tooltip("The rocket's position's pitch factor")] [SerializeField] float positionPitchFactor = -2f;
+    [Tooltip("The rocket's control's pitch factor")] [SerializeField] float controlPitchFactor = -10f;
 
-    [SerializeField] float controlRollFactor = -20f;
+    [Tooltip("The rocket's position's yaw factor")] [SerializeField] float positionYawFactor = 2f;
+
+    [Tooltip("The rocket's controls's roll factor")] [SerializeField] float controlRollFactor = -20f;
+
+    [Header("GameObjects")]
+    [Tooltip("The rocket's lasers")] [SerializeField] GameObject[] lasers;
 
     float zThrow;
     float yThrow;
@@ -24,18 +33,20 @@ public class PlayerControls : MonoBehaviour
     void OnEnable()
     {
         movement.Enable();
+        fire.Enable();
     }
 
     void OnDisable()
     {
         movement.Disable();
+        fire.Disable();
     }
 
     void Update()
     {
         ProcessTranslation();
         ProcessRotation();
-
+        ProcessFiring();
     }
 
     private void ProcessRotation()
@@ -65,5 +76,27 @@ public class PlayerControls : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, clampedZPos);
+    }
+
+    private void ProcessFiring()
+    {
+        if (fire.ReadValue<float>() > 0.5) {
+            SetLasersActive(true);
+        } else
+        {
+            SetLasersActive(false);
+        }
+
+    }
+
+    private void SetLasersActive(bool isActive)
+    {
+
+        foreach (GameObject laser in lasers)
+        {
+            var emmisionModule = laser.GetComponent<ParticleSystem>().emission;
+            emmisionModule.enabled = isActive;
+        }
+
     }
 }
